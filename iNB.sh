0f50d3c683b4983f1c8b32016cdcc8715d6d7c09
gutosie <gutosie@o2.pl>
2017-11-11 11:44:17 +0200

    Add files via upload

---
+++ iNB.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+#skrypt instaluje neoboot-a
+#
+[ -e /tmp/neoboot.zip ] && rm -f /tmp/neoboot.zip
+[ -e /tmp/neoboot-master ] && rm -rf /tmp/neoboot-master
+URL='https://github.com/gutosie/neoboot/archive/master.zip'
+curl -kLs $URL  -o /tmp/neoboot.zip
+cd /tmp/
+unzip -qn ./neoboot.zip
+rm -f /tmp/neoboot.zip
+#kopiowanie
+Cel="/usr/lib/enigma2/python/Plugins/Extensions"
+[ -e $Cel/NeoBoot ] && rm -rf $Cel/NeoBoot/* || mkdir -p $Cel/NeoBoot
+mv -f /tmp/neoboot-master/NeoBoot/* $Cel/NeoBoot
+[ -e /tmp/neoboot-master ] && rm -rf /tmp/neoboot-master
+cd /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/; chmod 0755 ./bin/*; chmod 0755 ./ex_init.py; chmod 0755 ./files/targetimage.sh; chmod 0755 ./files/NeoBoot.sh; chmod 0755 ./files/S50fat.sh; chmod 0755 ./bin/rebootbot; cd;                        
+echo "Restart GUI, aby zaladowac NeoBoot-a""
+sleep 5; killall -9 enigma2
+exit 0
