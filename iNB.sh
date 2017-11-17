e8fabaa962e22abeba10b8f5db6f667eed382aaf
gutosie <gutosie@o2.pl>
2017-11-17 12:00:59 +0200

    Update iNB.sh

--- iNB.sh
+++ iNB.sh
@@ -14,12 +14,15 @@ Cel="/usr/lib/enigma2/python/Plugins/Extensions"
 [ -e $Cel/NeoBoot ] && rm -rf $Cel/NeoBoot/* || mkdir -p $Cel/NeoBoot
 mv -f /tmp/neoboot-master/NeoBoot/* $Cel/NeoBoot
 [ -e /tmp/neoboot-master ] && rm -rf /tmp/neoboot-master
-cd /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/; chmod 0755 ./bin/*; chmod 0755 ./ex_init.py; chmod 0755 ./files/targetimage.sh; chmod 0755 ./files/NeoBoot.sh; chmod 0755 ./files/S50fat.sh; chmod 0755 ./bin/rebootbot; cd;                        
-echo "NeoBoot zainstalowany!!! Potrzebny restart GUI. Restartuje..."
+cd /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/; chmod 0755 ./bin/*; chmod 0755 ./ex_init.py; chmod 0755 ./files/targetimage.sh; chmod 0755 ./files/checkpoint.sh; chmod 0755 ./files/NeoBoot.sh; chmod 0755 ./files/S50fat.sh; chmod 0755 ./bin/rebootbot; cp /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/bin/neoinit* /sbin; cd;                        
+echo "NeoBoot zainstalowany!!!"
+echo ""
 echo "#####################################################"
 echo "#          NEOBOOT INSTALLED SUCCESSFULL            #"
 echo "#####################################################"
 echo "#             PLEASE RESTART YOUR STB               #"
 echo "#####################################################"
+echo ""
+echo "Potrzebny restart GUI. q(-_-)p Restartuje..."
 sleep 5; killall -9 enigma2
 exit 0
