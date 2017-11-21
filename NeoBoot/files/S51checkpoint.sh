3c5c2138aadadd1d4247f9e6134ca4e14d63fe5c
gutosie <gutosie@o2.pl>
2017-11-21 18:49:51 +0200

    dodano nowy plik

---
+++ NeoBoot/files/S51checkpoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# script gutosie
+                   
+if [ -f /checkpoint ] ; then                                                        
+    rm -f /checkpoint
+fi 
+echo ok                                                
