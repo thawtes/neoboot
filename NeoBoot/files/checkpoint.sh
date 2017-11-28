a592909437e6ab43e5efda42e6b28603515176a4
gutosie <gutosie@o2.pl>
2017-11-28 23:58:37 +0200

    Delete checkpoint.sh

--- NeoBoot/files/checkpoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# script gutosie
-                   
-if [ -f /checkpoint ] ; then                                                       
-    echo ok  
-    rm -f /checkpoint
-fi 
-echo ok                                                
