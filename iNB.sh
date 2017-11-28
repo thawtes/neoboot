e28f279e2f05e95f4e2149a5e11396a1c0ba78ea
gutosie <gutosie@o2.pl>
2017-11-28 22:01:20 +0200

    q(-_-)p

--- iNB.sh
+++ iNB.sh
@@ -16,7 +16,6 @@ mv -f /tmp/neoboot-master/NeoBoot/* $Cel/NeoBoot
 [ -e /tmp/neoboot-master ] && rm -rf /tmp/neoboot-master
 cd /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/; chmod 755 ./bin/*; chmod 755 ./ex_init.py; chmod 755 ./files/targetimage.sh; chmod 755 ./files/checkpoint.sh; chmod 755 ./files/NeoBoot.sh; chmod 755 ./files/S50fat.sh; chmod 755 /files/S51checkpoint.sh; chmod 755 ./bin/rebootbot; chmod 755 ./bin/nanddump; chmod 755 ./bin/nfidump; cd;                        
 chmod -R +x /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/ubi_reader/*
-echo "NeoBoot zainstalowany!!!"
 echo ""
 echo "#####################################################"
 echo "#          NEOBOOT INSTALLED SUCCESSFULL            #"
