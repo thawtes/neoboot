d0c3ab9ce37ef5f27e3ba24ff0cfa239c3dea500
gutosie <gutosie@o2.pl>
2017-11-28 23:56:21 +0200

    Update iNB.sh

--- iNB.sh
+++ iNB.sh
@@ -14,7 +14,7 @@ Cel="/usr/lib/enigma2/python/Plugins/Extensions"
 [ -e $Cel/NeoBoot ] && rm -rf $Cel/NeoBoot/* || mkdir -p $Cel/NeoBoot
 mv -f /tmp/neoboot-master/NeoBoot/* $Cel/NeoBoot
 [ -e /tmp/neoboot-master ] && rm -rf /tmp/neoboot-master
-cd /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/; chmod 755 ./bin/*; chmod 755 ./ex_init.py; chmod 755 ./files/targetimage.sh; chmod 755 ./files/checkpoint.sh; chmod 755 ./files/NeoBoot.sh; chmod 755 ./files/S50fat.sh; chmod 755 /files/S51checkpoint.sh; chmod 755 ./bin/rebootbot; chmod 755 ./bin/nanddump; chmod 755 ./bin/nfidump; cd;                        
+cd /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/; chmod 755 ./bin/*; chmod 755 ./ex_init.py; chmod 755 ./files/targetimage.sh; chmod 755 ./files/checkpoint.sh; chmod 755 ./files/NeoBoot.sh; chmod 755 ./files/S50fat.sh; chmod 755 ./files/S51checkpoint.sh; chmod 755 ./bin/rebootbot; chmod 755 ./bin/nanddump; chmod 755 ./bin/nfidump; cd;                        
 chmod -R +x /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/ubi_reader/*
 echo ""
 echo "#####################################################"
