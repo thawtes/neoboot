de18d5d325e5f943e91ed8379e53a389c4fefde4
gutosie <gutosie@o2.pl>
2017-12-14 18:39:43 +0200

    Delete S51checkpoint.sh

--- NeoBoot/files/S51checkpoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# script gutosie
-                   
-if [ -f /checkpoint ] ; then                                                        
-    rm -f /checkpoint
-fi 
-echo ok                                                
