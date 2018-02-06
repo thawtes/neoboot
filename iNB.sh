bfe644bc05843d2d095d4190c52da6a306877aea
Zdzislaw <zdzislaw22@windowslive.com>
2018-02-06 18:49:59 +0100

    i jeszcze raz drobna poprawka

--- iNB.sh
+++ iNB.sh
@@ -19,7 +19,7 @@ Cel="/usr/lib/enigma2/python/Plugins/Extensions"
 [ -e $Cel/NeoBoot ] && rm -rf $Cel/NeoBoot/* || mkdir -p $Cel/NeoBoot
 mv -f /tmp/neoboot-master/NeoBoot/* $Cel/NeoBoot
 [ -e /tmp/neoboot-master ] && rm -rf /tmp/neoboot-master
-cd /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/
+cd $Cel/NeoBoot
 chmod 755 ./bin/*
 chmod 755 ./ex_init.py
 chmod 755 ./files/*.sh
