73130bab577c84388be22924d849d0b542790d8b
gutosie <gutosie@o2.pl>
2018-04-04 18:09:17 +0200

    mała zmiana

--- NeoBoot/files/S50fat.sh
+++ NeoBoot/files/S50fat.sh
@@ -10,12 +10,15 @@ if [ -f /etc/init.d/inadyn-mt ] ; then
 fi
 
 if [ -f /usr/bin/oscam ] ; then
-    if ! ps | grep oscam | grep -v grep ; then /usr/bin/oscam -b; fi
-    /etc/init.d/softcam.oscam 
-fi
-
-if [ -f /usr/bin/CCcam ] ; then
-    /usr/bin/CCcam start
+    if ! ps | grep oscam | grep -v grep ; then 
+        /usr/bin/oscam -b 
+        echo "START OSCam.."        
+    fi
+elif [ -f /usr/bin/CCcam ] ; then
+    if ! ps | grep CCcam | grep -v grep ; then 
+        /usr/bin/CCcam stop || start || restart  
+        echo "START CCCam.."
+    fi
 fi
                                                 
 if [ -f /home/root/*.tar.gz ] ; then
@@ -31,12 +34,13 @@ if [ ! -e /media/hdd ] ; then
 fi
                                                 
 mount -a
-echo "file S50fat.sh delete"     
+   
                    
 if [ -f /etc/rcS.d/S50fat.sh ] ; then
                             ln -s /usr/lib/enigma2/python/Plugins/Extensions/NeoBoot/files/S50fat.sh /etc/rcS.d/S50neo.sh                                                        
                             telnetd on
                             echo ok  
                             rm -f /etc/rcS.d/S50fat.sh
+                            echo "file S50fat.sh delete"  
 fi 
 echo ok                                                
