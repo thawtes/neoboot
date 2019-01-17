cfdee0c093dc0c4944460aba2658400969de4a0a
gutosie <gutosie@o2.pl>
2019-01-17 08:58:43 +0200

    octagon sf8008 OK

--- NeoBoot/extract.py
+++ NeoBoot/extract.py
@@ -1021,12 +1021,8 @@ def RemoveUnpackDirs():
         rc = os.system('rm -r /media/neoboot/ImagesUpload/vuplus')
     elif os.path.exists('/media/neoboot/ImagesUpload/sf4008'):
         rc = os.system('rm -r /media/neoboot/ImagesUpload/sf4008')
-
-    elif os.path.exists('/media/neoboot/ImagesUpload/octagon/octagon/sf8008'):
-        rc = os.system('rm -r /media/neoboot/ImagesUpload/')          
-        rc = os.system('rm -r /media/neoboot/ImagesUpload/usb.update.bin')                  
-        rc = os.system('rm -r /media/neoboot/ImagesUpload/octagon') 
-                        
+    elif os.path.exists('/media/neoboot/ImagesUpload/octagon/sf8008'):          
+        rc = os.system('rm -r /media/neoboot/ImagesUpload/usb.update.bin; rm -r /media/neoboot/ImagesUpload/octagon')                        
     elif os.path.exists('/media/neoboot/ImagesUpload/dm900'):
         rc = os.system('rm -r /media/neoboot/ImagesUpload/dm900')
     elif os.path.exists('/media/neoboot/ImagesUpload/hd51'):
