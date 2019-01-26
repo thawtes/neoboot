169bac2d63c196d78968f1db45daeae38914a785
gutosie <gutosie@o2.pl>
2019-01-26 21:50:12 +0200

    module_neoboot.sh

---
+++ NeoBoot/files/module_neoboot.sh
@@ -0,0 +1,13 @@
+opkg update
+opkg install --force-reinstall mtd-utils
+opkg install --force-reinstall mtd-utils-ubifs
+opkg install --force-reinstall mtd-utils-jffs2
+opkg install --force-reinstall kernel-module-nandsim
+opkg install --force-reinstall python-subprocess
+opkg install --force-reinstall python-argparse
+opkg install --force-reinstall curl
+opkg install --force-reinstall liblzo2-2
+opkg install --force-reinstall python-imaging
+opkg install --force-maintainer --force-reinstall --force-overwrite kernel-image
+opkg configure update-modules
+cd
